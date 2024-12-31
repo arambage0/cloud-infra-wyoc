55045bf8de4d0ca91e7dff243129bcfaf7552e09
arambage0 <106842248+arambage0@users.noreply.github.com>
2024-12-31 06:18:08 +0530

    Update create-ec2.sh

--- scripts/provision-eks/create-ec2.sh
+++ scripts/provision-eks/create-ec2.sh
@@ -1,1 +1,8 @@
-Test: wyoc
+source ./ec2-config.properties
+
+console_message() {
+   local log_type=$1
+   local log_msg=$2
+   local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+   echo "${timestamp} [${log_type}] ${log_msg}"
+                }
